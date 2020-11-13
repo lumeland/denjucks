2c3b083d7cb1e4d3f2b0953f0dc73bf402cdceed
Oscar Otero <oom@oscarotero.com>
2020-11-13 19:44:59 +0100

    fixed asap dep failing

--- src/deps.js
+++ src/deps.js
@@ -1,6 +1,6 @@
-export * as path from "https://deno.land/std@0.61.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.77.0/path/mod.ts";
 
-export * as asap from "https://cdn.pika.dev/asap/^2.0.6";
+export * as asap from "./deps/asap.js";
 
 // @deno-types="./waterfall.d.ts"
 export { default as waterfall } from "https://jspm.dev/npm:a-sync-waterfall@1.0.1";
